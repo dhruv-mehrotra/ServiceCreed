3d03929066b5259a44a7ea3405a7682ab22d1a98
dhruv-mehrotra <dhruv_m@cs.iitr.ac.in>
2021-11-08 15:46:19 +0530

    feat: updated with google maps sdk

--- lib/viewmodels/nearby_services/map_model.dart
+++ lib/viewmodels/nearby_services/map_model.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -8,93 +7,88 @@ import 'package:service_creed/services/nearby_location_api.dart';
 
 class MapModel extends ChangeNotifier {
   GoogleMapController _mapController;
+  List<LatLng> users = [];
 
-  String userPositionName = '';
-  LatLng userPosition;
+  LatLng userPosition = LatLng(40.71427, -74.00597);
   List<Nearby> nearbyLocations = <Nearby>[];
 
   GoogleMapController get mapController => _mapController;
-  List<User> users = [];
 
   final Set<Marker> _markers = {};
 
   Set<Marker> get markers => _markers;
 
   MapModel(String service);
-}
 
-// Future<void> addAllUserMakerToMap() async {
-//   var allUser = await UserApi.instance.getAllUser();
-//   print("runin...");
-//   allUser.forEach((user) async {
-//     await _addMarkerToMap(
-//       markerPosition: GeoPoint(double.parse(user.address.geo.lat), double.parse(user.address.geo.lng)),
-//       placeName: '${user.address.city}',
-//     );
-//     users.add(user);
-//   });
-// }
-
-// Future<void> getUserPosition(User user) async {
-//   print(user.address.geo.lat);
-//   GeoPoint point = GeoPoint(double.parse(user.address.geo.lat), double.parse(user.address.geo.lng));
-//   userPosition = LatLng(point.latitude, point.longitude);
-//   List<Placemark> placeMarks = await placemarkFromCoordinates(userPosition.latitude, userPosition.longitude);
-//   Placemark marks = placeMarks[0];
-//   _addMarkerToMap(markerPosition: point, placeName: '${user.username}');
-//   userPositionName = '${marks.name},\n${marks.locality}, ${marks.administrativeArea}.';
-//   // _moveCameraToUserLocation(userPosition);
-// }
-
-Future<void> _addMarkerToMap({GeoPoint markerPosition, String placeName}) async {
-  try {
-    List<Placemark> placeMarks = await placemarkFromCoordinates(markerPosition.latitude, markerPosition.longitude);
-    Placemark marks = placeMarks[0];
-    var points = LatLng(markerPosition.latitude, markerPosition.longitude);
-    _markers.add(Marker(
-        markerId: MarkerId(placeName),
-        position: points,
-        onTap: () async {
-          final List<Nearby> result = await NearcyLocationApi.instance.getNearby(
-              userLocation:
-                  // markerPosition,
-                  GeoPoint(4.8119283, 7.046236272219636),
-              radius: 1000,
-              type: 'restaurants',
-              keyword: '');
-          nearbyLocations = result;
-
-          notifyListeners();
-        },
-        infoWindow: InfoWindow(
-          title: placeName,
-          snippet: "${marks.name},${marks.locality}, ${marks.administrativeArea}",
-        ),
-        icon: BitmapDescriptor.defaultMarker));
-  } catch (e) {
-    print(e);
+  Future<void> addAllUserMakerToMap() async {
+    await _addMarkerToMap(
+      markerPosition: GeoPoint(40.71427, -74.00597),
+    );
+    users.add(LatLng(40.71427, -74.00597));
   }
-  notifyListeners();
-}
+
+  Future<void> getUserPosition() async {
+    GeoPoint point = LatLng(40.71427, -74.00597) as GeoPoint;
+    userPosition = LatLng(40.71427, -74.00597);
+    List<Placemark> placeMarks =
+        await placemarkFromCoordinates(40.71427, -74.00597);
+    Placemark marks = placeMarks[0];
+    _addMarkerToMap(
+      markerPosition: point,
+    );
+  }
+
+  Future<void> _addMarkerToMap({GeoPoint markerPosition}) async {
+    try {
+      List<Placemark> placeMarks =
+          await placemarkFromCoordinates(40.71427, -74.00597);
+      Placemark marks = placeMarks[0];
+      var points = LatLng(markerPosition.latitude, markerPosition.longitude);
+      _markers.add(Marker(
+          position: points,
+          onTap: () async {
+            final List<Nearby> result =
+                await NearcyLocationApi.instance.getNearby(
+                    userLocation:
+                        // markerPosition,
+                        GeoPoint(4.8119283, 7.046236272219636),
+                    radius: 10000,
+                    type: 'restaurants',
+                    keyword: '');
+            nearbyLocations = result;
+
+            notifyListeners();
+          },
+          infoWindow: InfoWindow(
+            snippet:
+                "${marks.name},${marks.locality}, ${marks.administrativeArea}",
+          ),
+          icon: BitmapDescriptor.defaultMarker));
+    } catch (e) {
+      print(e);
+    }
+    notifyListeners();
+  }
 
   void onMapCreate(GoogleMapController controller) {
-    GoogleMapController _mapController = controller;
+    _mapController = controller;
     notifyListeners();
   }
 
   void onCameraMoved(CameraPosition position) {
-    LatLng userPosition = position.target;
+    userPosition = position.target;
     notifyListeners();
   }
+
 //
-//   void _moveCameraToUserLocation(LatLng newPosition) async {
-//     _mapController.animateCamera(
-//       CameraUpdate.newCameraPosition(
-//         CameraPosition(
-//           target: newPosition,
-//           zoom: 15,
-//         ),
-//       ),
-//     );
-//   }
-// }
+  void _moveCameraToUserLocation(LatLng newPosition) async {
+    _mapController.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: newPosition,
+          zoom: 15,
+        ),
+      ),
+    );
+  }
+}
